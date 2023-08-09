6d8b6fb92175e7e51893ccfdebf56baa70b94cb9
Galen <miguelherquez@gmail.com>
2023-08-08 19:10:18 -0600

    Adds CBV listView for pages.

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,15 @@
+from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
+from django.views.generic.list import ListView
 
-# Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    template_name = 'pages/pages.html'
+    model = Page
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        return context
 
 def page(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
