3f557b6e6632f51227f91b6badc864d0f266feed
Galen <miguelherquez@gmail.com>
2023-08-08 19:44:03 -0600

    Adds CBV for a detail view for pages.

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PageListView.as_view(), name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('<int:pk>/<slug:page_slug>/', views.PageDetailView.as_view(), name='page'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,12 @@
 from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
-from django.views.generic.list import ListView
+from django.views.generic import list, detail
 
-class PageListView(ListView):
+class PageListView(list.ListView):
     template_name = 'pages/pages.html'
     model = Page
 
-    def get_context_data(self, **kwargs: Any):
-        context = super().get_context_data(**kwargs)
-        return context
-
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageDetailView(detail.DetailView):
+    template_name = 'pages/page.html'
+    model = Page
