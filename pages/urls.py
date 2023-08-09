2fea1fb1d463051365bb90487d4c44d15ec9a788
Galen <miguelherquez@gmail.com>
2023-08-08 20:39:33 -0600

    Adds pages_patters and modifys urls in templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+pages_patterns = ([
     path('', views.PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:page_slug>/', views.PageDetailView.as_view(), name='page'),
-]
+    path('create/', views.PageCreateView.as_view(), name='create'),
+], 'pages')
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('core.urls')),
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
 ]
