d0d6d6d95461dc3e66db17b584a56a2d459bc8ae
Galen <miguelherquez@gmail.com>
2023-08-08 21:04:12 -0600

    Adds create view, form and corrects url.

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
-from typing import Any, Dict
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
 from django.views.generic import list, detail
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class PageListView(list.ListView):
     model = Page
 
 class PageDetailView(detail.DetailView):
     model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+
+    success_url = reverse_lazy('pages:pages')
