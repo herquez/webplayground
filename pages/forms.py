21b70953915f021ba1d7bfcd1bb344d33af2d71a
Galen <miguelherquez@gmail.com>
2023-08-11 19:42:24 -0600

    Change order and style of the form.

--- pages/forms.py
+++ pages/forms.py
@@ -4,9 +4,17 @@ from .models import Page
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
-        fields = ['title', 'content', 'order']
+        fields = ['order', 'title', 'content']
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control'}),
-            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'title': forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder': 'Título',
+            }),
+            'content': forms.Textarea(attrs={ 'class':'form-control' }),
             'order': forms.NumberInput(attrs={'class':'form-control'}),
+        }
+        labels = {
+            'title': '',
+            'content': '',
+            'order': '',
         }
