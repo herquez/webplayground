306d295ad6edec200128e895ea6390c82eed585c
Galen <miguelherquez@gmail.com>
2023-08-11 20:18:02 -0600

    Adds Mixins to redirect in case of no staff user.

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,19 @@
+from typing import Any
 from .models import Page
 from django.views.generic import list, detail
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .forms import PageForm
+
+class StaffRequiredMixin(object):
+    """
+    Mixin that verifys that user is staff
+    """
+    def dispatch(self, request, *args: Any, **kwargs: Any):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super().dispatch(request, *args, **kwargs)
 
 class PageListView(list.ListView):
     model = Page
@@ -10,13 +21,13 @@ class PageListView(list.ListView):
 class PageDetailView(detail.DetailView):
     model = Page
 
-class PageCreateView(CreateView):
+class PageCreateView(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
-class PageUpdateView(UpdateView):
+class PageUpdateView(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -24,7 +35,7 @@ class PageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id])+'?ok'
     
-class PageDeleteView(DeleteView):
+class PageDeleteView(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
     
