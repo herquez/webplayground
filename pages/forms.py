c8b4b51f6cd72edfaf39ed8a14256f1b8b418199
Galen <miguelherquez@gmail.com>
2023-08-11 19:03:01 -0600

    Adds ckeditor dependencies. Adds widgets to form fields.

--- pages/forms.py
+++ pages/forms.py
@@ -5,3 +5,8 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = ['title', 'content', 'order']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'order': forms.NumberInput(attrs={'class':'form-control'}),
+        }
